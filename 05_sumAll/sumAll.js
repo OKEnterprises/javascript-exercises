aac7f8d840c5525140a3d8d8a9b0c9f43c0c0834
Garret O'Connor <okenerenterprises@gmail.com>
2022-12-23 12:49:50 -0500

    Completed 05 Sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+function smaller(a, b) {
+    if (a > b) return b;
+    else return a;
+}
 
+function larger(a, b) {
+    if (a > b) return a;
+    else return b;
+}
+
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    let sum = 0;
+    let start = smaller(a, b);
+    let end = larger(a, b);
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
