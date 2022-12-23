1a5570811bf26252ba26daec79ec79a032ddfd5c
Garret O'Connor <okenerenterprises@gmail.com>
2022-12-23 13:12:49 -0500

    Completed 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const reverseString = function(string) {
+    let charArr = string.split("");
+    return charArr.reverse().join("");
+};
 
+function stripString(string) {
+    let punctuation = ['!', ',', '.', ' '];
+
+    console.log(string.split("").filter(char => !punctuation.includes(char)).join(""));
+
+    return string.split("").filter(char => !punctuation.includes(char)).join("");
+}
+
+const palindromes = function (string) {
+    let stripped = stripString(string).toLowerCase()
+    return reverseString(stripped) === stripped;
 };
 
 // Do not edit below this line
