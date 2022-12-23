7f1775fede43672033619b32cc9a27a38a608438
Garret O'Connor <okenerenterprises@gmail.com>
2022-12-23 12:56:45 -0500

    Completed 07 Temp Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function roundToTens(number) {
+  return Math.round(number * 10) / 10
+}
+
+const convertToCelsius = function(degF) {
+  return roundToTens((5 / 9) * (degF - 32));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  return roundToTens((degC * (9 / 5)) + 32);
 };
 
 // Do not edit below this line
