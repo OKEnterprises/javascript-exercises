bc494f7d191784380c2986eb7ef8c8bc6f527045
Garret O'Connor <okenerenterprises@gmail.com>
2022-12-23 12:34:10 -0500

    Completed 02 Repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, numTimes) {
+    if (numTimes < 0) return "ERROR";
+    let out = "";
+    for (let i = 0; i < numTimes; i++) {
+        out += string;
+    }
+    return out;
 };
 
 // Do not edit below this line
