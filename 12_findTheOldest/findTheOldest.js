0643133a6f7dd2957a2c16be5acf3db8033e6b78
Garret O'Connor <okenerenterprises@gmail.com>
2022-12-23 13:35:02 -0500

    Completed 12 Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldestAge = 0;
+    let oldestPerson;
+    array.forEach(person => {
+        let upperBound = person.yearOfDeath || new Date().getFullYear();
+        let age = upperBound - person.yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
