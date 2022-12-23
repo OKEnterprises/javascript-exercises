fb3a67901722bc2f238cf623634daf9d5110f3bd
Garret O'Connor <okenerenterprises@gmail.com>
2022-12-23 13:03:31 -0500

    Completed 08 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(item => {
+    sum += item;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach(item => {
+    product = product * item;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	let product = 1;
+  for (let i = 0; i < exp; i++) {
+    product = product * base;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) return 1;
+  else return a * factorial(a - 1);
 };
 
 // Do not edit below this line
