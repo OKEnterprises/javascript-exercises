c10df9bea11103bc38771f45ce3ab14c665673a1
Garret O'Connor <okenerenterprises@gmail.com>
2022-12-23 13:20:00 -0500

    Completed 10 Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return 'OOPS';
+    let counter = 1;
+    let prevNum = 0;
+    let currNum = 1;
+    while (counter < index) {
+        let temp = currNum;
+        currNum = currNum + prevNum;
+        prevNum = temp;
+        counter++;
+    }
+    return currNum;
 };
 
 // Do not edit below this line
