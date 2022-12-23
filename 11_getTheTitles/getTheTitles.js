69014cdbae239404b1e03606ccc122414b5c888e
Garret O'Connor <okenerenterprises@gmail.com>
2022-12-23 13:23:03 -0500

    Completed 11 Get the Titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let out = [];
+    arr.forEach(book => {
+        out.push(book.title);
+    });
+    return out;
 };
 
 // Do not edit below this line
